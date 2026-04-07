a89c866df7e717aa0a5b5d5c4c60efed2caff120
이태호 <th20001026@gmail.com>
2026-04-07 14:32:38 +0900

    refactor: CT_5_1_3_P5 Set을 XOR로
    XOR 은 토글 기능을 할 수 있다.
    Set 대신 사용 가능!

--- CodeTree/5/CT_5_1_3_P5.java
+++ CodeTree/5/CT_5_1_3_P5.java
@@ -1,10 +1,8 @@
 import java.util.ArrayDeque;
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Queue;
 import java.util.Scanner;
-import java.util.Set;
 
 public class CT_5_1_3_P5 {
     public static void main(String[] args) {
@@ -16,21 +14,24 @@ public class CT_5_1_3_P5 {
         G = sc.nextInt();
 
         // 각 사람이 가진 그룹을 의미
-        List<Set<Integer>>[] p = new List[N + 1];
+        List<Integer>[] p = new List[N + 1];
         boolean[] visited = new boolean[N+1];
+
+        int[] remain = new int[G];
+        int[] xor = new int[G];
 
         for (int i = 1; i <= N; i++) {
             p[i] = new ArrayList<>();
         }
 
         // 각 사람이 가지고 있는 그룹을 입력받음
-        for (int i = 1; i <= G; i++) {
+        for (int g = 0; g < G; g++) {
             int size = sc.nextInt();
-            Set<Integer> group = new HashSet<>();
+            remain[g] = size;
             for (int j = 0; j < size; j++) {
                 int person = sc.nextInt();
-                p[person].add(group);
-                group.add(person);
+                p[person].add(g);
+                xor[g] ^= person;
             }
         }
 
@@ -42,10 +43,11 @@ public class CT_5_1_3_P5 {
         while (!q.isEmpty()) {
             int now = q.poll();
 
-            for (Set<Integer> g : p[now]) {
-                g.remove(now);
-                if (g.size() == 1) {
-                    Integer left = g.iterator().next();
+            for (Integer g : p[now]) {
+                xor[g] ^= now;
+                remain[g]--;
+                if (remain[g] == 1) {
+                    Integer left = xor[g];
                     // q에 없을 경우에만 집어 넣자.
                     if (!visited[left]) {
                         q.add(left);
